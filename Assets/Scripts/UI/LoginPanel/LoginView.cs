d67464b775d1f2a6cb522474802708fd7585b01e
zhanglutao-314 <17332098459@163.com>
2025-06-18 21:46:41 +0800

    修改

--- Assets/Scripts/UI/LoginPanel/LoginView.cs
+++ Assets/Scripts/UI/LoginPanel/LoginView.cs
@@ -12,7 +12,7 @@ public class LoginView : ViewBase
         loginBtn = uiWindow.transform.Find("LoginBtn").GetComponent<Button>();
         loginBtn.onClick.AddListener(() =>
         {
-            GameScenesManager.Instance.LoadSceneAsync("Game", "GamePanel");
+            GameScenesManager.Instance.LoadSceneAsync("Game", "LoginPanel");
         });
     }
 }
